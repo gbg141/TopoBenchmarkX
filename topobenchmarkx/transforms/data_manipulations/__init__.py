dfabc2e29ec9a83734f3298c29bb16f8d23198b9
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-09-19 14:45:44 +0200

    ADD: Precompute embeddings for SANN

--- topobenchmarkx/transforms/data_manipulations/__init__.py
+++ topobenchmarkx/transforms/data_manipulations/__init__.py
@@ -12,6 +12,7 @@ from .keep_selected_data_fields import KeepSelectedDataFields
 from .node_degrees import NodeDegrees
 from .node_features_to_float import NodeFeaturesToFloat
 from .one_hot_degree_features import OneHotDegreeFeatures
+from .precompute_khop_features import PrecomputeKHopFeatures
 
 DATA_MANIPULATIONS = {
     "Identity": IdentityTransform,
@@ -24,6 +25,7 @@ DATA_MANIPULATIONS = {
     "CalculateSimplicialCurvature": CalculateSimplicialCurvature,
     "KeepOnlyConnectedComponent": KeepOnlyConnectedComponent,
     "KeepSelectedDataFields": KeepSelectedDataFields,
+    "PrecomputeKHopFeatures": PrecomputeKHopFeatures,
 }
 
 __all__ = [
@@ -37,5 +39,6 @@ __all__ = [
     "CalculateSimplicialCurvature",
     "OneHotDegreeFeatures",
     "KeepSelectedDataFields",
+    "PrecomputeKHopFeatures",
     "DATA_MANIPULATIONS",
 ]
